9bb7c5e054fa32addf636c32dbcce2050b402478
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-04-23 19:45:04 +0900

    feat: 기본 기능 구현

---
+++ content.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  applyVisibilitySettings();
+});
+
+function applyVisibilitySettings() {
+  chrome.storage.local.get("visibilitySettings", (data) => {
+    const settings = data.visibilitySettings || {};
+    Object.keys(settings).forEach((selector) => {
+      const isVisible = settings[selector];
+      const elements = document.querySelectorAll(selector);
+      elements.forEach((element) => {
+        element.style.visibility = isVisible ? "visible" : "hidden";
+      });
+    });
+  });
+}
--- popup.js
+++ popup.js
@@ -1,20 +1,35 @@
-document
-  .getElementById("toggleVisibility")
-  .addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (tab) {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: toggleElementsVisibility,
-      });
-    }
-  });
+const visibilityToggles = {
+  togglePartTitleBreadcrumb: ".part-title, .breadcrumb",
+  toggleLevel: "td.level",
+  toggleFinishedCount: "td.finished-count",
+  toggleAcceptanceRate: "td.acceptance-rate",
+};
 
-function toggleElementsVisibility() {
-  const elementsToHide = document.querySelectorAll(
-    ".part-title, .breadcrumb, .level, .finished-count, .acceptance-rate"
-  );
-  elementsToHide.forEach((element) => {
+Object.entries(visibilityToggles).forEach(([buttonId, selector]) => {
+  document.getElementById(buttonId).addEventListener("click", () => {
+    updateVisibilitySettings(selector);
+  });
+});
+
+function updateVisibilitySettings(selector) {
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    await chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: toggleElementsVisibility,
+      args: [selector],
+    });
+
+    chrome.storage.local.get("visibilitySettings", (data) => {
+      const settings = data.visibilitySettings || {};
+      settings[selector] = !settings[selector];
+      chrome.storage.local.set({ visibilitySettings: settings });
+    });
+  });
+}
+
+function toggleElementsVisibility(selector) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((element) => {
     element.style.visibility =
       element.style.visibility === "hidden" ? "visible" : "hidden";
   });
