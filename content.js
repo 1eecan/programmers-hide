bfbf89016b3c1b733189327620a08817354baa49
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-04-24 16:12:39 +0900

    feat: 기본 기능 완성
    * DOM 불러오기 전에 visibility:hidden으로 하는 기능 추가 전

--- content.js
+++ content.js
@@ -1,16 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  applyVisibilitySettings();
-});
+const elementVisibility = {
+  ".part-title, .breadcrumb": false,
+  "td.level": false,
+  "td.finished-count": false,
+  "td.acceptance-rate": false,
+};
 
-function applyVisibilitySettings() {
-  chrome.storage.sync.get("visibilitySettings", (data) => {
-    const settings = data.visibilitySettings || {};
-    Object.keys(settings).forEach((selector) => {
-      const isVisible = settings[selector];
-      const elements = document.querySelectorAll(selector);
-      elements.forEach((element) => {
-        element.style.visibility = isVisible ? "visible" : "hidden";
-      });
+function loadSettings() {
+  chrome.storage.local.get("visibilitySettings", (data) => {
+    const initialVisibility = data.visibilitySettings
+      ? data.visibilitySettings
+      : elementVisibility;
+
+    if (data.visibilitySettings) {
+      console.log("Loaded from storage");
+    } else {
+      console.log("Cant loaded from storage");
+    }
+
+    Object.keys(initialVisibility).forEach((selector) => {
+      updateVisibility(selector, initialVisibility[selector]);
     });
   });
 }
+
+function updateVisibility(selector, isHidden) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach((element) => {
+    element.style.visibility = isHidden ? "hidden" : "visible";
+  });
+}
+
+let timeout = null;
+
+const observer = new MutationObserver((mutations) => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() => {
+    loadSettings();
+    document.body.style.visibility = "visible";
+  }, 100);
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
--- popup.js
+++ popup.js
@@ -5,32 +5,46 @@ const visibilityToggles = {
   toggleAcceptanceRate: "td.acceptance-rate",
 };
 
-Object.entries(visibilityToggles).forEach(([buttonId, selector]) => {
-  document.getElementById(buttonId).addEventListener("click", () => {
-    updateVisibilitySettings(selector);
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get("visibilitySettings", function (data) {
+    if (data.visibilitySettings) {
+      document.getElementById("togglePartTitleBreadcrumb").checked =
+        data.visibilitySettings[".part-title, .breadcrumb"];
+      document.getElementById("toggleLevel").checked =
+        data.visibilitySettings["td.level"];
+      document.getElementById("toggleFinishedCount").checked =
+        data.visibilitySettings["td.finished-count"];
+      document.getElementById("toggleAcceptanceRate").checked =
+        data.visibilitySettings["td.acceptance-rate"];
+    }
   });
 });
 
-function updateVisibilitySettings(selector) {
+Object.entries(visibilityToggles).forEach(([buttonId, selector]) => {
+  document.getElementById(buttonId).addEventListener("change", (event) => {
+    const isHidden = event.target.checked;
+    updateVisibilitySettings(selector, isHidden);
+  });
+});
+
+function updateVisibilitySettings(selector, isHidden) {
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
     await chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
       function: toggleElementsVisibility,
-      args: [selector],
-    });
-
-    chrome.storage.sync.get("visibilitySettings", (data) => {
-      const settings = data.visibilitySettings || {};
-      settings[selector] = !settings[selector];
-      chrome.storage.sync.set({ visibilitySettings: settings });
+      args: [selector, isHidden],
     });
   });
 }
 
-function toggleElementsVisibility(selector) {
+function toggleElementsVisibility(selector, isHidden) {
   const elements = document.querySelectorAll(selector);
   elements.forEach((element) => {
-    element.style.visibility =
-      element.style.visibility === "hidden" ? "visible" : "hidden";
+    element.style.visibility = isHidden ? "hidden" : "visible";
+  });
+  chrome.storage.local.get("visibilitySettings", (data) => {
+    const settings = data.visibilitySettings || {};
+    settings[selector] = isHidden;
+    chrome.storage.local.set({ visibilitySettings: settings });
   });
 }
