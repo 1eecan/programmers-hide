d338af643852f91ff87d4f2247e9aa4a5a256690
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-04-24 16:56:43 +0900

    feat: 버전 1.0
    * 문제 유형, 레벨, 푼사람, 정답률 가리는 기능 추가
    * 프로그래머스 이외의 페이지에서 토글 버튼 보이지 않게 하는 기능 추가

--- popup.js
+++ popup.js
@@ -6,6 +6,23 @@ const visibilityToggles = {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
+  // 프로그래머스 문제 풀이 사이트인지 확인하는 로직
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTabUrl = tabs[0].url;
+    if (currentTabUrl.includes("school.programmers.co.kr")) {
+      document.getElementById("toggleContainer").style.display = "block";
+      document.getElementById("otherSite").style.display = "none";
+    } else {
+      document.getElementById("toggleContainer").style.display = "none";
+      document.getElementById("otherSite").style.display = "block";
+    }
+  });
+  // 깃허브 링크로 이동시키는 로직
+  var link = document.getElementById("github");
+  link.addEventListener("click", function () {
+    chrome.tabs.create({ url: this.href });
+  });
+  // 로컬에서 상태 불러와서 popup에 반영하는 로직
   chrome.storage.local.get("visibilitySettings", function (data) {
     if (data.visibilitySettings) {
       document.getElementById("togglePartTitleBreadcrumb").checked =
